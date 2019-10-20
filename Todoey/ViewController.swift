0d48d8eec6f010bd251902e27d57a00fd21e3a2a
Erik Hafnor <erikhafnor@gmail.com>
2019-10-20 21:29:28 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Erik Hafnor on 20.10.2019.
+//  Copyright © 2019 Erik Hafnor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
