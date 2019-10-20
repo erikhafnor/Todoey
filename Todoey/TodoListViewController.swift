e161a689abcae2fa05cf664b3a5c6b5d850af17f
Erik Hafnor <erikhafnor@gmail.com>
2019-10-20 22:21:57 +0200

    Complete and functional with add item-feature, before .defaults (persistent data)

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Egg", "Bacon", "Komler"]
+    var itemArray = ["Egg", "Bacon", "Komler"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,5 +48,37 @@ class TodoListViewController: UITableViewController {
         
     }
 
+    //MARK - Add new items
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on our UIAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+            
+            
+        }
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+        
+     }
+        
 }
+    
+
 
