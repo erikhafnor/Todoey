6bc5b9ffd24639c8e945af5f139de27080144e60
Erik Hafnor <erikhafnor@gmail.com>
2019-12-20 11:57:42 +0100

    Category tableview challenge done

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -10,27 +10,106 @@ import UIKit
 import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var categoryArray = [Category]()
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        loadCategories()
 
     }
     
      //MARK: - TableView Datasource Methods
     //Set up our datasource to show all tableviews in our persistent container
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCategoryCell", for: indexPath)
+        
+        let category = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        
+        return cell
+        
+    }
 
      //MARK: - Data Manipulation Methods
     
     // Set up save data, load data so we can use CRUD
+   
+    func saveCategories() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
     
     
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        // method with internal and external parameters, with request,
+        //and also has a default value ,= Item.fetchRequest()
+        
+        do {
+            categoryArray = try context.fetch(request)
+            
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+  
+   
      //MARK: - Add New Categories
     
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        var textField = UITextField()
         
+        let alert = UIAlertController(title: "Add New Todoey Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            //what will happen once the user clicks the add item button on our UIAlert
+            
+            
+            
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text!
+            
+            
+            self.categoryArray.append(newCategory)
+            
+            self.saveCategories()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new category"
+            textField = alertTextField
+            
+            
+            
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
         
     }
     
@@ -39,7 +118,7 @@ class CategoryViewController: UITableViewController {
     
      //MARK: - TableView Delegate Methods
     
-    
+  
     
     
 }
