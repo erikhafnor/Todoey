125429709f0dccb6197cd620464c1dfd8c4a66b1
Erik Hafnor <erikhafnor@gmail.com>
2019-12-20 10:48:47 +0100

    prosjektet fungerer. satt opp til challenge om å selv sette opp categoryTableView.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Erik Hafnor on 19.12.2019.
+//  Copyright © 2019 Erik Hafnor. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+     //MARK: - TableView Datasource Methods
+    //Set up our datasource to show all tableviews in our persistent container
+
+
+     //MARK: - Data Manipulation Methods
+    
+    // Set up save data, load data so we can use CRUD
+    
+    
+     //MARK: - Add New Categories
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        
+        
+    }
+    
+   
+    
+    
+     //MARK: - TableView Delegate Methods
+    
+    
+    
+    
+}
+
