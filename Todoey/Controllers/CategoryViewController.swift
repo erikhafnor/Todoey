3b13cce9d26401902c5218fd60aba0b02865dcde
Erik Hafnor <erikhafnor@gmail.com>
2020-01-13 11:54:23 +0100

    Swipeable cells to delete in categoryView

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -104,8 +104,6 @@ class CategoryViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
             //what will happen once the user clicks the add item button on our UIAlert
             
-            
-            
             let newCategory = Category()
             newCategory.name = textField.text!
             
@@ -117,23 +115,12 @@ class CategoryViewController: UITableViewController {
             alertTextField.placeholder = "Create new category"
             textField = alertTextField
             
-            
-            
-        }
+            }
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
     }
-    
-   
-    
-    
-
-    
-  
-    
-    
 }
 
 //MARK: - Swipecell delegate methods
@@ -145,7 +132,18 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
         
         let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
             // handle action by updating model with deletion
-            print("Item deleted")
+            
+            if let categoryForDeletion = self.categories?[indexPath.row] {
+            
+            do {
+            try self.realm.write {
+                self.realm.delete(categoryForDeletion)
+            }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+                
+            }
         }
         
         // customize the action appearance
@@ -153,4 +151,12 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
         
         return [deleteAction]
 }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+
+        return options
+    }
+    
 }
